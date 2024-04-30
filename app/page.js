0d8c2f2ef17b05fe4c53b06d740857b2054d290c
LokeshKarthik5 <34629208+lokeshkarthik5@users.noreply.github.com>
2024-04-30 14:05:07 +0530

    done

--- app/page.js
+++ app/page.js
@@ -18,12 +18,16 @@ export default function Home() {
       <div className="relative mt-10 mb-4">
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
         <div className="relative z-10 p-4 text-white text-center">
-          <h2 className="text-2xl font-bold">Welcome to the Summit</h2>
+          <h2 className="text-2xl font-bold">Welcome to Summit</h2>
           <p className="text-lg">A place to learn, build, and grow your skills</p>
         </div>
       </div>
-      <Image src={logo} className="w-full" alt="Pic" />
-
+      <Image src={logo} className="w-full relative" alt="Pic" />
+      <div className="absolute mt-[40%] text-white text-xl font-bold">
+        <h1 className="w-[700px]">Welcome to our Platform and explore our various tools to help with your
+        educational needs </h1>
+        <h1 className="w-[700px] mt-5">Simply <span className="text-black">Sign Up</span> with us to use the tools</h1>
+      </div>
       
     </main>
   );
